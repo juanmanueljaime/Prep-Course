4d755bd4377ffc63baae9daca091495c5919befc
juanmanueljaime <105234876+juanmanueljaime@users.noreply.github.com>
2022-06-28 18:42:57 -0300

    test-terminado-vs1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -171,17 +171,9 @@ function elevar(num, exponent) {
   return math.pow (num, exponent);
 }
 
-function redondearNumero(num) {
-  // Redondea "num" al entero más próximo y devuélvelo
-  // Tu código:
-  
-  return math.pow (num, exponent);
+function redondearNumero(...args: [num: any]) {
 
-}
-
-function redondearNumero(num) {
-  
-return math.round(num);
+  return math.round(num);
 }
 
 function redondearHaciaArriba(num) {
