5ba86b151f1c4451141bbc4bec666d578e254467
juanmanueljaime <105234876+juanmanueljaime@users.noreply.github.com>
2022-07-12 20:11:01 -0300

    primer commit jsII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -191,7 +191,7 @@ function tablaDelSeis(){
   //Escribe tu código aquí   
   let arrayTablaDel6 = []
   for (let i = 0; i < 11; i++) {
-    arrayTablaDel6.push (6 * 1)
+        arrayTablaDel6.push(6 * i)
   }
   return arrayTablaDel6
 }
